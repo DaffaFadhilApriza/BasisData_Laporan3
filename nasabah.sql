d90d6b6f134826b172ce579ec3d806bb91846504
DaffaFadhilApriza <131609626+DaffaFadhilApriza@users.noreply.github.com>
2023-09-22 20:41:17 +0700

    Create nasabah.sql

---
+++ nasabah.sql
@@ -0,0 +1,11 @@
+CREATE TABLE nasabah (
+  id_nasabah INT(10) PRIMARY KEY,
+  no_rekening_nasabah INT(16),
+  nama VARCHAR(20),
+  no_telepon INT(12),
+  alamat VARCHAR(20),
+  email VARCHAR(50)
+  tanggal_lahir DATE,
+  jenis_kelamin ENUM('Laki-laki','Perempuan')
+  pekerjaan VARCHAR(20)
+);

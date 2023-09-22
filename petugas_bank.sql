b01a334e52af327d0dce40ca8007cd609f68c616
DaffaFadhilApriza <131609626+DaffaFadhilApriza@users.noreply.github.com>
2023-09-22 21:19:08 +0700

    Update petugas_bank.sql

--- petugas_bank.sql
+++ petugas_bank.sql
@@ -1,6 +1,6 @@
 CREATE TABLE (
   id_petugas INT(10) PRIMARY KEY,
-  cabang_petugas VARCHAR(20),
+  cabang_petugas VARCHAR(20) UNIQUE,
   nama VARCHAR(20),
   jabatan VARCHAR(20),
   jenis_kelamin ENUM('Laki-laki', 'Perempuan'),

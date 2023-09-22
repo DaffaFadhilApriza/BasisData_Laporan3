645dc286b10d42b746ee56eab7a48d48535e7214
DaffaFadhilApriza <131609626+DaffaFadhilApriza@users.noreply.github.com>
2023-09-22 20:47:12 +0700

    Update nasabah.sql

--- nasabah.sql
+++ nasabah.sql
@@ -4,8 +4,8 @@ CREATE TABLE nasabah (
   nama VARCHAR(20),
   no_telepon INT(12),
   alamat VARCHAR(20),
-  email VARCHAR(50)
+  email VARCHAR(50),
   tanggal_lahir DATE,
-  jenis_kelamin ENUM('Laki-laki','Perempuan')
+  jenis_kelamin ENUM('Laki-laki','Perempuan'),
   pekerjaan VARCHAR(20)
 );

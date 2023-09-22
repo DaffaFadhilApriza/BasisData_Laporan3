0fce040e6c5f36a3e71c086cd037be2ff379edd6
DaffaFadhilApriza <131609626+DaffaFadhilApriza@users.noreply.github.com>
2023-09-22 21:11:24 +0700

    Update rekening.sql

--- rekening.sql
+++ rekening.sql
@@ -1,6 +1,6 @@
 CREATE TABLE (
   no_rekening INT(16) PRIMARY KEY,
-  nama_pemilik_rekening VARCHAR(20),
+  nama_pemilik_rekening VARCHAR(20) UNIQUE,
   jenis_rekening VARCHAR(20),
   saldo INT(50)
 );
